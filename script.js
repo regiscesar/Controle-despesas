7bd11eb93cb7f13ebddb8e5b84b574818e72c0e0
regiscesar <69260016+regiscesar@users.noreply.github.com>
2022-02-04 14:05:04 -0300

    Controle de gastos

---
+++ script.js
@@ -0,0 +1,11 @@
+const dummyTransaction = [
+    { id: 1, name: "despesa", amount: -50 },
+    { id: 2, name: "salario", amount: 3500 },
+    { id: 3, name: "extra", amount: 100 },
+    { id: 4, name: "despesa", amount: -50 },
+    { id: 5, name: "despesa", amount: -50 }
+]
+
+const addTransactionIntoDOM = transaction => {
+
+}
